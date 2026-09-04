6d41fd3a8b2b120367b24962f468719b5f17e16a
01015-joshua <ys.2myleisure@gmail.com>
2026-09-04 12:39:00 +0900

    Update dashboard

--- publish_dashboard.bat
+++ publish_dashboard.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 
 copy /Y "outputs\US_Sector_Q2_2026_Interactive_Dashboard_v7_3_fundamentals_v98_preview.html" "public\index.html"
-git add public\index.html README.md .gitignore .github\workflows\pages.yml publish_dashboard.bat download_dashboard_from_github.bat
+git add public\index.html .gitignore .github\workflows\pages.yml publish_dashboard.bat
 git commit -m "Update dashboard"
 git push
 
